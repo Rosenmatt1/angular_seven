83be02aeca055682db6db2b4671249574066f3ab
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-02-25 12:00:13 -0700

    Created some basic functions with button and event listener

---
+++ westeros/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title:string = 'westeros';
+  season_8:boolean = false
+
+  characters:string[] = ["Tyrian", "Deanerys", "The Hound", "Aria"]
+
+counter:number = 0;
+
+bump() {
+  this.counter++
+}
+
+reveal() {
+  this.season_8 = !this.season_8
+}
+
+
+}
